a745c799560442ffabc2426544255684742576a9
JB-GoodLife <jb@goodlife.dk>
2025-04-07 17:29:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -184,7 +184,10 @@ def main_app():
             )
         with col3:
         # Calculate button
-        submitted = st.form_submit_button("Beregning", on_click=_calculate_cb, args=(input_fields,))
+        submitted = st.form_submit_button(
+            "Beregning", 
+            on_click=_calculate_cb, 
+            args=(input_fields,)
             )            
 
     # Display calculation results if calculation is done
